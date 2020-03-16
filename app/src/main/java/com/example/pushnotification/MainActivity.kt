19db900828a1a565c2e7d6c946445c86fc0639c1
Azizjon <zzdehkanov@gmail.com>
2020-03-16 15:52:29 +0500

    test

---
+++ app/src/main/java/com/example/pushnotification/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pushnotification
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e("Azizjon","test")
+    }
+}
